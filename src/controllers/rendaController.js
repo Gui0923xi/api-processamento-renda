9b724ef8af893bbe1957a7b67c01d4713111b00f
Gui0923xi <guilhermeoliveleal@gmail.com>
2024-11-19 22:14:40 -0300

    Update rendaController.js

--- src/controllers/rendaController.js
+++ src/controllers/rendaController.js
@@ -1,5 +1,14 @@
 const { formatarRenda } = require('../utils/formatters');
 
 exports.padronizarRenda = (renda) => {
-    return formatarRenda(renda);
+    if (!renda) return { error: "Renda é obrigatória." };
+
+    // Divida os valores por vírgula e processe cada um
+    const rendas = renda.split(',').map((item) => item.trim());
+    const rendasPadronizadas = rendas.map(formatarRenda);
+
+    return {
+        rendaOriginal: rendas,
+        rendasPadronizadas
+    };
 };
