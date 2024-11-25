170f2db906310ada2fe9814a577c1817790ddc67
Gui0923xi <guilhermeoliveleal@gmail.com>
2024-11-25 14:35:16 -0300

    Create db.js

---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mysql = require('mysql2');
+
+// Criar conexão com o banco de dados
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
+});
+
+// Exportar a conexão para uso nos controladores
+module.exports = pool.promise();
